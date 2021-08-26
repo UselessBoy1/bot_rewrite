64ac949e41b5026afe8cdb9c03c8e7aaf2dcd0e9
UselessBoy <useless.boy.v@protonmail.com>
2021-08-26 17:52:22 +0200

    changed run command in web dyno from python to gunicorn

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,10 @@ import asyncio
 
 from threading import Thread
 from app.main import app
+from tools import misc
 
 if __name__ == "__main__":
+    misc.log("RUNNING SERVER")
     loop = asyncio.get_event_loop()
     Thread(target=bot.bot_run, args=(loop, )).start()
     app.run()
