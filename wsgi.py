97e5aab0c5c125e1c294b42ea02177fb3964e799
UselessBoy <useless.boy.v@protonmail.com>
2021-08-26 19:00:30 +0200

    fixed event loop in bot thread v2

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,9 @@ import asyncio
 from threading import Thread
 from app.main import app
 
+policy = asyncio.get_event_loop_policy()
+policy._loop_factory = asyncio.SelectorEventLoop
+
 if __name__ == "__main__":
     app_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(app_loop)
