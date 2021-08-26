a8797a0cdebe092477d642edd9a0930c6b28bf83
UselessBoy <useless.boy.v@protonmail.com>
2021-08-26 19:25:20 +0200

    added port env

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 import bot
 import asyncio
+import os
 
 from threading import Thread
 from app.main import app
@@ -7,9 +8,11 @@ from app.main import app
 policy = asyncio.get_event_loop_policy()
 policy._loop_factory = asyncio.SelectorEventLoop
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
     app_loop = asyncio.new_event_loop()
     asyncio.set_event_loop(app_loop)
     loop = asyncio.new_event_loop()
     Thread(target=bot.run_bot, args=(loop,)).start()
-    app.run()
+    app.run(port=port)
