b32a2f3cdecab983549e8197c77565e24e592c41
UselessBoy <useless.boy.v@protonmail.com>
2021-08-26 18:52:59 +0200

    fixed event loop in bot thread

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,6 @@ from threading import Thread
 from app.main import app
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     Thread(target=bot.run_bot, args=(loop,)).start()
     app.run()
