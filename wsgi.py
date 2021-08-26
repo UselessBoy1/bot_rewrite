c1c380ca4b5bf5a44e36cc41b8f8733d442f46ae
UselessBoy <useless.boy.v@protonmail.com>
2021-08-26 17:54:32 +0200

    testing gunicorn

--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,9 @@ from threading import Thread
 from app.main import app
 from tools import misc
 
+misc.log("RUNNING SERVER")
+loop = asyncio.get_event_loop()
+Thread(target=bot.bot_run, args=(loop,)).start()
+
 if __name__ == "__main__":
-    misc.log("RUNNING SERVER")
-    loop = asyncio.get_event_loop()
-    Thread(target=bot.bot_run, args=(loop, )).start()
     app.run()
